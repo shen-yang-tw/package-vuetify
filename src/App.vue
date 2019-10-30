7dbd5b3ff01ac5db87eb700aabd300c31cce299d
Shen Yang <shen.yang.work.tw@gmail.com>
2019-10-30 14:23:12 +0800

    1030

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,40 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-app-bar>
 
     <v-content>
+      <BreadCrumb :items="val.items" isBlue="val.isBlue" msg="val.msg"/>
+      <!-- or <BreadCrumb :items="[{disabled: false, href: 'breadcrumbs_dashboard', text: 'Link1'}, {disabled: true, href: 'breadcrumbs_dashboard', text: 'Link2'}]" isBlue msg="Text"/> -->
+      <!-- 'items' must use [v-bind:] -->
       <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
+/*
 import HelloWorld from './components/HelloWorld';
+import BreadCrumb from './components/BreadCrumb';
+*/
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HelloWorld: () => import('@/components/HelloWorld.vue'), /* webpackChunkName: 'hello-world' */
+    BreadCrumb: () => import('@/components/BreadCrumb.vue'),
   },
   data: () => ({
-    //
+    val: {
+      items: [
+        {disabled: false, href: 'breadcrumbs_dashboard', text: 'Link1'}, {disabled: true, href: 'breadcrumbs_dashboard', text: 'Link2'}
+      ],
+      isBlue: true,
+      msg: "Text"
+    }
   }),
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
+//Set this to false to prevent the production tip on Vue startup: https://vuejs.org/v2/api/index.html#productionTip
 Vue.config.productionTip = false
 
 new Vue({
